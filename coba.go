ad13373104c6d44df31c1780e042790318e3a142
PengedarDonut <dimasputras2006@gmail.com>
2025-11-22 20:35:53 +0700

    belajar variabel dan daga types

--- coba.go
+++ coba.go
@@ -3,7 +3,23 @@ package main
 import "fmt"
 
 func main() {
-   fmt.Println("Hello World")
+    // TIPE 1: Deklarasi Tertib (Pake 'var')
+    // Bagus buat variabel global atau yang butuh tipe data spesifik
+    var nama string = "Pengedar Donut"
+    var umur int = 20
+
+    // TIPE 2: Deklarasi Santuy (Short Declaration)
+    // Pake ':=', Go otomatis nebak tipe datanya.
+    // Cuma bisa dipake di DALAM function.
+    hobi := "Push Rank Github"
+    isJago := true
+
+    // Printf = Print Format (buat nyelipin variabel ke teks)
+    // %s = string, %d = digit (angka), %t = boolean (true/false)
+    fmt.Printf("Nama: %s\n", nama)
+    fmt.Printf("Umur: %d tahun\n", umur)
+    fmt.Printf("Hobi: %s\n", hobi)
+    fmt.Printf("Status Jago: %t\n", isJago)
 }
 
-
+ 
